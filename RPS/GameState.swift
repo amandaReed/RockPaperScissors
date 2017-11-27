7d9ec00668dcaac26c63a7803209b419610eada5
amandaReed <cis290@PC22761.local>
2017-11-27 17:31:47 -0600

    RockPaperScissors
    RockPaperScissors

---
+++ RPS/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Amanda Reed on 11/27/17.
+//  Copyright © 2017 RockValleyCollege. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
